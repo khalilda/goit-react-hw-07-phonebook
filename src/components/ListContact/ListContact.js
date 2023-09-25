fa4f2d1ad652e39954997aadd61a790115106f62
khalilda <100872154+khalilda@users.noreply.github.com>
2023-09-25 09:09:51 +0300

    hw

--- src/components/ListContact/ListContact.js
+++ src/components/ListContact/ListContact.js
@@ -1,3 +1,47 @@
+// import ListModule from './ListContact.module.css';
+// import { useDispatch, useSelector } from 'react-redux';
+// import AddIcon from 'components/Icons/AddIcon';
+// import { getFiltered } from 'components/redux/contactsSelector';
+// import { deleteContact } from 'components/redux/backendAPI';
+
+// export const ListContact = () => {
+//   const contacts = useSelector(getFiltered);
+//   const dispatch = useDispatch();
+//   const filter = useSelector(getFiltered);
+
+//   const handleDelete = id => {
+//     dispatch(deleteContact(id));
+
+//     const filteredContacts = contacts.filter(object =>
+//       object.name.toLowerCase().trim().includes(filter)
+//     );
+
+//     return (
+//       <ul className={ListModule.list}>
+//         {contacts && contacts.length > 0 ? (
+//           filteredContacts.map(({ id, name, number }) => (
+//             <li key={id} className={ListModule.item}>
+//               <button
+//                 type="button"
+//                 className={ListModule.removeButton}
+//                 onClick={() => handleDelete(id)}
+//                 aria-label="Delete contact"
+//               >
+//                 <AddIcon width="45" height="45" />
+//               </button>
+//               {name + ': ' + number}
+//             </li>
+//           ))
+//         ) : (
+//           <p className={ListModule.alert}>
+//             Sorry! You have no contacts created.
+//           </p>
+//         )}
+//       </ul>
+//     );
+//   };
+// };
+
 import ListModule from './ListContact.module.css';
 import { useDispatch, useSelector } from 'react-redux';
 import AddIcon from 'components/Icons/AddIcon';
@@ -7,37 +51,31 @@ import { deleteContact } from 'components/redux/backendAPI';
 export const ListContact = () => {
   const contacts = useSelector(getFiltered);
   const dispatch = useDispatch();
-  const filter = useSelector(getFiltered);
+  // const filter = useSelector(getFiltered);
 
   const handleDelete = id => {
     dispatch(deleteContact(id));
-
-    const filteredContacts = contacts.filter(object =>
-      object.name.toLowerCase().trim().includes(filter)
-    );
-
-    return (
-      <ul className={ListModule.list}>
-        {contacts && contacts.length > 0 ? (
-          filteredContacts.map(({ id, name, number }) => (
-            <li key={id} className={ListModule.item}>
-              <button
-                type="button"
-                className={ListModule.removeButton}
-                onClick={() => handleDelete(id)}
-                aria-label="Delete contact"
-              >
-                <AddIcon width="45" height="45" />
-              </button>
-              {name + ': ' + number}
-            </li>
-          ))
-        ) : (
-          <p className={ListModule.alert}>
-            Sorry! You have no contacts created.
-          </p>
-        )}
-      </ul>
-    );
   };
+
+  return (
+    <ul className={ListModule.list}>
+      {contacts && contacts.length > 0 ? (
+        contacts.map(({ id, name, number }) => (
+          <li key={id} className={ListModule.item}>
+            <button
+              type="button"
+              className={ListModule.removeButton}
+              onClick={() => handleDelete(id)}
+              aria-label="Delete contact"
+            >
+              <AddIcon width="45" height="45" />
+            </button>
+            {name + ': ' + number}
+          </li>
+        ))
+      ) : (
+        <p className={ListModule.alert}>Sorry! You have no contacts created.</p>
+      )}
+    </ul>
+  );
 };
